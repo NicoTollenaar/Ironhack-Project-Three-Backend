59cb7abb5fe484b88b9963aa7a79fddb295548eb
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-03-08 10:43:20 +0100

    committing before pushing to heroku

--- db/connectDatabase.js
+++ db/connectDatabase.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./../../.env" });
+
 const mongoose = require("mongoose");
 
 // line below somehow not working, temporarily hardcode mongoDb into mongoose.connect(), fix later
-const mongoDb = process.env.MONGODB_REMOTE_URL || process.env.MONG0DB_LOCAL_URL;
+const mongoDb = process.env.MONGODB_REMOTE_URI || process.env.MONG0DB_LOCAL_URI;
 
 async function connectDatabase() {
   try {
     console.log("In connectDatabase, logging mongodb :", mongoDb);
-    const connection = await mongoose.connect(
-      "mongodb://localhost/chainAccount"
-    );
+    const connection = await mongoose.connect(mongoDb);
     console.log(
       `Connected to ${connection.connections[0]._connectionString}\nhost: ${connection.connections[0].host}\nport: ${connection.connections[0].port}\nname: ${connection.connections[0].name}`
     );
