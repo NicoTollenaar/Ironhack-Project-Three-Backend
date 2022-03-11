1d9d3cc5cde176a121e5f2a34d24de802e421fff
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-03-11 11:25:11 +0100

    seem to have solved cors issue

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const morgan = require("morgan");
 require("./db/connectDatabase");
 const PORT = process.env.PORT || 4001;
 
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin:
-//       process.env.ORIGIN || "http://localhost:3000",
-//   })
-// );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
 
+app.use(
+  cors({
+    credentials: true,
+    origin:
+      process.env.ORIGIN || "http://localhost:3000",
+  })
+);
 
 app.use(morgan("dev"));
 
@@ -24,19 +29,13 @@ app.use(express.static("./public"));
 
 const authRoutes = require("./routes/authRoutes.js");
 app.use(
-  "/",   
-  cors({
-    origin: ["https://chainaccount.netlify.app"],
-  }), 
+  "/", 
   authRoutes
 );
 
 const accountRoutes = require("./routes/accountRoutes");
 app.use(
   "/",
-  cors({
-    origin: ["https://chainaccount.netlify.app"],
-  }),  
   accountRoutes);
 
 const serverSideEventRoutes = require("./routes/serverSideEventRoutes");
