4ba55bb8433281515c5f4980306e1390d99cda6d
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-03-12 16:49:13 +0100

    moved setheaders after cors in serverjs

--- server.js
+++ server.js
@@ -6,23 +6,24 @@ const morgan = require("morgan");
 require("./db/connectDatabase");
 const PORT = process.env.PORT || 4001;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
 
 
 app.use(
   cors({
     credentials: true,
     origin:
-      process.env.ORIGIN || "http://localhost:3000",
+    process.env.ORIGIN || "http://localhost:3000",
     allowheaders: ["Origin, X-Requested-With, Content-Type, Accept", "Authorization" ],
   })
-);
-
-app.use(morgan("dev"));
+  );
+  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+  
+  app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
